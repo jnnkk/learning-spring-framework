f77a1cc0633c795f6c922886f977357ae86c0c26
SJY <jun000405@naver.com>
2024-01-03 21:10:59 +0900

    feat: 0103 Sec2 Complete(qualifier, primary)

--- src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.helloworld;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person (String name, int age, Address address) {};
 record Address (String firstLine, String city) {};
@@ -25,14 +27,26 @@ public class HelloWorldConfiguration {
         return new Person(name(), age(), address()); // name, age
     }
     @Bean
-    public Person person3Parameters(String name, int age, Address address2) { // name, age, address2, 파라미터로 주입
-        return new Person(name, age, address2); // name, age
+    public Person person3Parameters(String name, int age, Address address3) { // name, age, address2, 파라미터로 주입
+        return new Person(name, age, address3); // name, age
     }
+    @Bean
+    @Primary
+    public Person person4Parameters(String name, int age, Address address) { // name, age, address2, 파라미터로 주입
+        return new Person(name, age, address); // name, age
+    }
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) { // name, age, address2, 파라미터로 주입
+        return new Person(name, age, address); // name, age
+    }
+
     @Bean(name = "address2")
+    @Primary
     public Address address() {
         return new Address("GwangJu", "Chumdan");
     }
     @Bean(name = "address3")
+    @Qualifier("address3qualifier")
     public Address address3() {
         return new Address("Sinan", "Chunsik");
     }
