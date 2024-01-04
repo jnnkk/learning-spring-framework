b1e24fe1bd01de654b07ca91258e385aff6b07bd
SJY <jun000405@naver.com>
2024-01-04 20:41:55 +0900

    feat: 0104 Sec3 Complete (Dependency Injection, Component)

---
+++ src/main/java/com/in28minutes/learnspringframework/examples/c1/MysqlDbDataService.java
@@ -0,0 +1,10 @@
+package com.in28minutes.learnspringframework.examples.c1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MysqlDbDataService implements DataService{
+    public int[] retrieveData() {
+        return new int[] { 1, 2, 3, 4, 5 };
+    }
+}
